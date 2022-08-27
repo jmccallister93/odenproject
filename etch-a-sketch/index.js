0acbca09d1d28a1c832445c0ae14387505dbac63
jmccallister93 <jaredmccallister93@gmail.com>
2022-08-27 14:24:35 -0700

    got the pen wokring

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -9,19 +9,29 @@ btnContainer.appendChild(clearBtn);
 
 let gridContainer = document.createElement('div');
 gridContainer.className = 'grid-container'
-
 document.body.appendChild(gridContainer);
 
-row = 256
-for( let i=0; i < row; i++){
-    let gridSquare = document.createElement('div');
-    gridSquare.className = 'grid-square' 
-    gridContainer.append(gridSquare);
-    gridSquare.addEventListener("mouseover", (e) => {
-        gridSquare.style.backgroundColor = 'black';
-    });
+
+let row = 256
+function createGrid(row){
+    for( let i=0; i < row; i++){
+        let gridSquare = document.createElement('div');
+        gridSquare.className = 'grid-square' 
+        gridContainer.append(gridSquare);
+        calc = Math.sqrt(row)
+        gridContainer.style = `grid-template-columns: repeat(${calc},1fr)`;
+        gridSquare.addEventListener("mouseover", (e) => {
+            gridSquare.style.backgroundColor = 'black';
+        });
+    }
 }
 
-// let clearBtn = document.createElement('button');
-// clearBtn.className = 'clear-btn';
-// document.body.appendChild(clearBtn);
+createGrid(row);
+
+clearBtn.addEventListener('click', (e) => {
+    // newRow = prompt("Enter new grid dimensions.");
+    // newRow = parseInt(newRow);
+    // row = newRow * newRow;
+    // createGrid(row);
+});
+
